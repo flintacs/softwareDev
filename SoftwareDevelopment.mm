42867ea6d0bd2587a3ab57604baea0d1bd4e3911
Andrey Koshelev <koshelev.andrey@gmail.com>
2020-03-29 17:22:02 +0300

    Add files via upload
    First version created

---
+++ SoftwareDevelopment.mm
@@ -0,0 +1,119 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1585478939021" ID="ID_1776422280" MODIFIED="1585489846817" TEXT="Software Development">
+<node CREATED="1585485917009" FOLDED="true" ID="ID_750891335" MODIFIED="1585490589467" POSITION="left" TEXT="Development Process">
+<icon BUILTIN="full-1"/>
+<node CREATED="1585487345572" ID="ID_812827887" LINK="https://agilemanifesto.org/" MODIFIED="1585487447676" TEXT="Manifesto for Agile Software Development"/>
+<node CREATED="1585485970306" ID="ID_1025987219" LINK="https://www.amazon.com/Art-Agile-Development-Pragmatic-Software/dp/0596527675" MODIFIED="1585485996032" TEXT="The Art of Agile Development by James Shore, Shane Warden"/>
+<node CREATED="1585486920955" ID="ID_1620497355" LINK="https://www.amazon.com/Essential-Scrum-Practical-Addison-Wesley-Signature-ebook/dp/B008NAKA5O/ref=sr_1_1?keywords=essential+scrum&amp;qid=1585486908&amp;s=books&amp;sr=1-1" MODIFIED="1585486999551" TEXT="Essential Scrum: A Practical Guide to the Most Popular Agile Process by Kenneth S. Rubin"/>
+</node>
+<node CREATED="1585479010365" FOLDED="true" ID="ID_658798748" MODIFIED="1585490588039" POSITION="left" TEXT="Design Basics">
+<icon BUILTIN="full-2"/>
+<node CREATED="1585480216503" FOLDED="true" ID="ID_1241136404" MODIFIED="1585487468803" TEXT="Modularity and OOP Principles">
+<node CREATED="1585483181457" ID="ID_395974092" LINK="https://www.amazon.com/Object-Oriented-Software-Construction-Book-CD-ROM/dp/0136291554" MODIFIED="1585483519042" TEXT="Object-Oriented Software Construction (2nd Edition) by  Bertrand Meyer"/>
+</node>
+<node CREATED="1585479757158" FOLDED="true" ID="ID_1756433466" MODIFIED="1585488481349" TEXT="Design by contract">
+<node CREATED="1585483181457" ID="ID_1168236650" LINK="https://www.amazon.com/Object-Oriented-Software-Construction-Book-CD-ROM/dp/0136291554" MODIFIED="1585483519042" TEXT="Object-Oriented Software Construction (2nd Edition) by  Bertrand Meyer"/>
+</node>
+<node CREATED="1585479542644" ID="ID_1091276164" MODIFIED="1585488483396" TEXT="SOLID">
+<node CREATED="1585483388112" ID="ID_1517274808" LINK="https://www.amazon.com/Software-Development-Principles-Patterns-Practices/dp/0135974445/ref=sr_1_6?keywords=Robert+Martin&amp;qid=1585483361&amp;s=books&amp;sr=1-6" MODIFIED="1585483424959" TEXT="Agile Software Development, Principles, Patterns, and Practices  by Robert C. Martin"/>
+<node CREATED="1585484076554" ID="ID_49132435" LINK="https://www.amazon.com/Clean-Architecture-Craftsmans-Software-Structure-ebook/dp/B075LRM681/ref=sr_1_1?keywords=clean+architecture&amp;qid=1585484064&amp;s=books&amp;sr=1-1" MODIFIED="1585484115468" TEXT="Clean Architecture: A Craftsman&apos;s Guide to Software Structure and Design  by Robert C. Martin"/>
+<node CREATED="1585483181457" ID="ID_179227136" LINK="https://www.amazon.com/Object-Oriented-Software-Construction-Book-CD-ROM/dp/0136291554" MODIFIED="1585488734325" TEXT="Object-Oriented Software Construction (2nd Edition) by  Bertrand Meyer">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This book is a primary source on Open Closed Principle and Design by Contract (which has much in common with Liskov Substitution Principle)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1585479828166" ID="ID_709466483" MODIFIED="1585488961850" TEXT="Clean Code">
+<node CREATED="1585479416405" ID="ID_541145217" LINK="https://www.amazon.com/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882" MODIFIED="1585483400667" TEXT="Clean Code: A Handbook of Agile Software Craftsmanship by Robert C. Martin">
+<arrowlink DESTINATION="ID_541145217" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1366040591" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_541145217" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1366040591" SOURCE="ID_541145217" STARTARROW="None" STARTINCLINATION="0;0;"/>
+</node>
+<node CREATED="1585483181457" ID="ID_949072708" LINK="https://www.amazon.com/Object-Oriented-Software-Construction-Book-CD-ROM/dp/0136291554" MODIFIED="1585483519042" TEXT="Object-Oriented Software Construction (2nd Edition) by  Bertrand Meyer"/>
+</node>
+<node CREATED="1585479786533" ID="ID_260884197" MODIFIED="1585488968082" TEXT="GoF Patterns">
+<node CREATED="1585483029952" ID="ID_1350057845" LINK="https://www.amazon.com/Design-Patterns-Elements-Reusable-Object-Oriented-dp-0201633612/dp/0201633612/ref=mt_hardcover?_encoding=UTF8&amp;me=&amp;qid=" MODIFIED="1585483096464" TEXT="Design Patterns: Elements of Reusable Object-Oriented Software by Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides"/>
+</node>
+<node CREATED="1585479839406" ID="ID_1380492798" MODIFIED="1585488970958" TEXT="Refactoring">
+<node CREATED="1585483289145" ID="ID_1705585383" LINK="https://www.amazon.com/Refactoring-Improving-Existing-Addison-Wesley-Signature/dp/0134757599" MODIFIED="1585483339812" TEXT="Refactoring: Improving the Design of Existing Code (2nd Edition) by Martin Fowler"/>
+<node CREATED="1585484177864" ID="ID_1417537503" LINK="https://www.amazon.com/Working-Effectively-Legacy-Code-EFFECT-ebook/dp/B005OYHF0A" MODIFIED="1585484213229" TEXT="Working Effectively with Legacy Code by Michael Feathers"/>
+</node>
+<node CREATED="1585479847838" ID="ID_484408243" MODIFIED="1585488979006" TEXT="Unit and Integration testing">
+<node CREATED="1585483590586" ID="ID_1575560134" LINK="https://www.amazon.com/Growing-Object-Oriented-Software-Guided-Tests/dp/0321503627" MODIFIED="1585483633818" TEXT="Growing Object-Oriented Software, Guided by Tests by  Steve Freeman, Nat Pryce"/>
+<node CREATED="1585483665384" ID="ID_1850673734" LINK="https://www.amazon.com/xUnit-Test-Patterns-Refactoring-Addison-Wesley-ebook/dp/B004X1D36K/ref=sr_1_1?crid=3SO4PNA63UTAG&amp;keywords=xunit+test+patterns&amp;qid=1585483647&amp;s=books&amp;sprefix=xUnit+Test+%2Cstripbooks-intl-ship%2C287&amp;sr=1-1" MODIFIED="1585483706820" TEXT="xUnit Test Patterns: Refactoring Test Code by Gerard Meszaros"/>
+<node CREATED="1585484555449" ID="ID_1605539611" LINK="https://martinfowler.com/articles/mocksArentStubs.html" MODIFIED="1585484597806" TEXT="Mocks Aren&apos;t Stubs: Article by Martin Fowler"/>
+<node CREATED="1585484662034" ID="ID_159298158" LINK="https://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html" MODIFIED="1585484693031" TEXT="TDD Harms Architecture: Article by Robert C. Martin"/>
+<node CREATED="1585484711681" ID="ID_467967540" LINK="https://blog.cleancoder.com/uncle-bob/2017/10/03/TestContravariance.html" MODIFIED="1585484752623" TEXT="Test Contra-variance: Article by Robert C. Martin"/>
+</node>
+</node>
+<node CREATED="1585479084869" FOLDED="true" ID="ID_1835202548" MODIFIED="1585490586343" POSITION="left" TEXT="Module/Service Design">
+<icon BUILTIN="full-3"/>
+<node CREATED="1585479693973" ID="ID_769790711" MODIFIED="1585489004786" TEXT="Enterprise Application Patterns">
+<node CREATED="1585484983560" ID="ID_1135528084" LINK="https://www.amazon.com/Patterns-Enterprise-Application-Architecture-Martin/dp/0321127420" MODIFIED="1585485001857" TEXT="Patterns of Enterprise Application Architecture by Martin Fowler"/>
+</node>
+<node CREATED="1585479706198" ID="ID_558359591" MODIFIED="1585490568650" TEXT="Tactical DDD">
+<node CREATED="1585485051394" ID="ID_1232110446" LINK="https://www.amazon.com/Domain-Driven-Design-Tackling-Complexity-Software/dp/0321125215" MODIFIED="1585485067804" TEXT="Domain-Driven Design: Tackling Complexity in the Heart of Software by Eric Evans"/>
+<node CREATED="1585485124186" ID="ID_947610998" LINK="https://www.amazon.com/Implementing-Domain-Driven-Design-Vaughn-Vernon/dp/0321834577" MODIFIED="1585485147910" TEXT="Implementing Domain-Driven Design by Vaughn Vernon"/>
+</node>
+<node CREATED="1585485239193" ID="ID_101328434" MODIFIED="1585489017693" TEXT="Hexagonal Architecture">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Known also as &quot;Ports and Adpaters&quot; or &quot;Clean&quot; Architecture
+    </p>
+    <p>
+      Browse also articles of its original author - Alistair Cockburn
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1585485124186" ID="ID_1157230675" LINK="https://www.amazon.com/Implementing-Domain-Driven-Design-Vaughn-Vernon/dp/0321834577" MODIFIED="1585485147910" TEXT="Implementing Domain-Driven Design by Vaughn Vernon"/>
+<node CREATED="1585484076554" ID="ID_1254096135" LINK="https://www.amazon.com/Clean-Architecture-Craftsmans-Software-Structure-ebook/dp/B075LRM681/ref=sr_1_1?keywords=clean+architecture&amp;qid=1585484064&amp;s=books&amp;sr=1-1" MODIFIED="1585484115468" TEXT="Clean Architecture: A Craftsman&apos;s Guide to Software Structure and Design  by Robert C. Martin"/>
+</node>
+</node>
+<node CREATED="1585479273717" FOLDED="true" ID="ID_1331983333" MODIFIED="1585490704768" POSITION="right" TEXT="Distributed Systems Design">
+<icon BUILTIN="full-4"/>
+<node CREATED="1585479721678" ID="ID_755644256" MODIFIED="1585490669551" TEXT="Strategic DDD">
+<node CREATED="1585485051394" ID="ID_81001968" LINK="https://www.amazon.com/Domain-Driven-Design-Tackling-Complexity-Software/dp/0321125215" MODIFIED="1585485067804" TEXT="Domain-Driven Design: Tackling Complexity in the Heart of Software by Eric Evans"/>
+<node CREATED="1585485124186" ID="ID_766815011" LINK="https://www.amazon.com/Implementing-Domain-Driven-Design-Vaughn-Vernon/dp/0321834577" MODIFIED="1585485147910" TEXT="Implementing Domain-Driven Design by Vaughn Vernon"/>
+</node>
+<node CREATED="1585479640062" ID="ID_549674782" MODIFIED="1585489037317" TEXT="Enterprise Integration Patterns">
+<node CREATED="1585485494217" ID="ID_36107718" LINK="https://www.amazon.com/Enterprise-Integration-Patterns-Designing-Deploying/dp/0321200683" MODIFIED="1585485534446" TEXT="Enterprise Integration Patterns: Designing, Building, and Deploying Messaging Solutions by Gregor Hohpe, Bobby Woolf"/>
+</node>
+<node CREATED="1585486786947" ID="ID_609745906" MODIFIED="1585489049806" TEXT="Service Design Patterns">
+<node CREATED="1585486823627" ID="ID_420126389" LINK="https://www.amazon.com/Service-Design-Patterns-Fundamental-Addison-Wesley-ebook/dp/B0061LAM5K/ref=sr_1_1?keywords=service+design+patterns&amp;qid=1585486806&amp;s=books&amp;sr=1-1" MODIFIED="1585486862465" TEXT="Service Design Patterns: Fundamental Design Solutions for SOAP/WSDL and RESTful Web Services by Robert Daigneau"/>
+</node>
+<node CREATED="1585479679732" ID="ID_1030047623" MODIFIED="1585485555470" TEXT="Stability Patterns">
+<node CREATED="1585485652745" ID="ID_1787279" LINK="https://www.amazon.com/Release-Design-Deploy-Production-Ready-Software-ebook/dp/B079YWMY2V/ref=sr_1_1?keywords=release+it&amp;qid=1585485639&amp;s=books&amp;sr=1-1" MODIFIED="1585485679266" TEXT="Release It!: Design and Deploy Production-Ready Software 2nd Edition by Michael T. Nygard"/>
+<node CREATED="1585485708506" ID="ID_218445296" LINK="https://www.amazon.com/Microservices-Patterns-Examples-Java/dp/B07ZFZQCNY/ref=sr_1_1?keywords=microservices+patterns&amp;qid=1585485693&amp;s=books&amp;sr=1-1" MODIFIED="1585485749592" TEXT="Microservices Patterns: With Examples in Java by Chris Richardson"/>
+</node>
+<node CREATED="1585479668540" ID="ID_1459931515" MODIFIED="1585488316490" TEXT="Microservices Patterns">
+<node CREATED="1585485708506" ID="ID_1292925246" LINK="https://www.amazon.com/Microservices-Patterns-Examples-Java/dp/B07ZFZQCNY/ref=sr_1_1?keywords=microservices+patterns&amp;qid=1585485693&amp;s=books&amp;sr=1-1" MODIFIED="1585485749592" TEXT="Microservices Patterns: With Examples in Java by Chris Richardson"/>
+<node CREATED="1585485824819" ID="ID_167900928" LINK="https://www.amazon.com/Monolith-Microservices-Evolutionary-Patterns-Transform-ebook/dp/B081TKSSNN/ref=sr_1_1?crid=2ZVHZOHADXO2M&amp;keywords=monolith+to+microservices&amp;qid=1585485814&amp;sprefix=monoli%2Caps%2C276&amp;sr=8-1" MODIFIED="1585485853722" TEXT="Monolith to Microservices: Evolutionary Patterns to Transform Your Monolith  by Sam Newman"/>
+<node CREATED="1585485776578" ID="ID_1387084523" LINK="https://www.amazon.com/Building-Microservices-Designing-Fine-Grained-Systems/dp/1491950358/ref=sr_1_1?keywords=building+microservices&amp;qid=1585485762&amp;s=audible&amp;sr=1-1-catcorr" MODIFIED="1585485804045" TEXT="Building Microservices: Designing Fine-Grained Systems by Sam Newman"/>
+</node>
+<node CREATED="1585479658573" ID="ID_1455998182" MODIFIED="1585489080663" TEXT="Data Streams">
+<node CREATED="1585485591562" ID="ID_595101472" LINK="https://www.amazon.com/Heart-Logs-Stream-Processing-Integration-ebook/dp/B00NUGHIU6/ref=sr_1_1?keywords=i+love+logs&amp;qid=1585485566&amp;s=books&amp;sr=1-1" MODIFIED="1585485624348" TEXT="I Heart Logs: Event Data, Stream Processing, and Data Integration by Jay Kreps"/>
+<node CREATED="1585486526379" ID="ID_1056871796" LINK="https://www.amazon.com/Streaming-Data-Understanding-real-time-pipeline/dp/1617292281/ref=sr_1_2?keywords=streaming+data&amp;qid=1585486540&amp;s=books&amp;sr=1-2" MODIFIED="1585486579987" TEXT="Streaming Data: Understanding the real-time pipeline by Andrew Psaltis"/>
+</node>
+<node CREATED="1585487389612" ID="ID_1196106071" LINK="https://12factor.net/" MODIFIED="1585487411800" TEXT="12 Factor Apps"/>
+</node>
+<node CREATED="1585487046612" FOLDED="true" ID="ID_165923351" MODIFIED="1585490701677" POSITION="right" TEXT="Continious Delivery">
+<arrowlink DESTINATION="ID_165923351" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1347873064" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_165923351" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1347873064" SOURCE="ID_165923351" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="full-5"/>
+<node CREATED="1585486042635" ID="ID_1728545027" LINK="https://www.amazon.com/Continuous-Delivery-Deployment-Automation-Addison-Wesley/dp/0321601912" MODIFIED="1585486070236" TEXT="Continuous Delivery: Reliable Software Releases through Build, Test, and Deployment Automation by Jez Humble, David Farley"/>
+</node>
+</node>
+</map>
